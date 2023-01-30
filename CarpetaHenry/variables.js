2c019dfc20930b3ac3af01e80f9d7ac3dfaf1273
koda_0 <jmtenorio1013@hotmail.com>
2023-01-29 19:47:29 -0500

    vartypes

---
+++ CarpetaHenry/variables.js
@@ -0,0 +1,18 @@
+//String
+var bootcamp = 'Henry';
+//Int
+var quant = 123;
+
+var neg = -24;
+
+var dec = 3.1415;
+//Bool
+var happy = false;
+
+var angry = true;
+//Undefined
+var box1;
+    console.log(box1);
+
+//Null
+var mt = null;
