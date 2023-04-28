8f5d1b73446c25e1a91074115d4a78493bea5c53
Kodama01 <jmtenorio1013@hotmnail.com>
2023-04-28 16:27:51 -0500

    m

--- CarpetaHenry/test001.js
+++ CarpetaHenry/test001.js
@@ -1,9 +1,8 @@
-  let arguments = [5,5]
-  if (arguments.length === 0){
-      console.log(0);
-   }   else if (arguments.length === 1){  
-      console.log(arguments[0]);
-   }  else if (arguments.length >= 2){
-      let mult = arguments.reduce((acc,currVal) => acc * currVal);
-      console.log(mult);
-   }
+if (arguments.length === 0){
+   return(0);
+}   else if (arguments.length === 1){  
+   return(arguments[0]);
+}  else if (arguments.length >= 2){
+   let mult = arguments.reduce((acc,currVal) => acc * currVal);
+   return(mult);
+}
