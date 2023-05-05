cdd17363f0cc9ff854265867c4581df1fff19f91
Kodama01 <jmtenorio1013@hotmail.com>
2023-05-05 10:48:23 -0500

    na

--- CarpetaHenry/tests2.js
+++ CarpetaHenry/tests2.js
@@ -1,4 +1,17 @@
-for (let i = 1; i <=4; i++){
-    console.log(`iteracion ${i}`);
-}
-console.log('Fin.');
+function stringMasLarga(strings) {
+    // La función llamada 'stringMasLarga', recibe como argumento un arreglo de strings llamado 'strings'
+    // y debe devolver el string más largo que hay en el arreglo (Es decir el de mayor cantidad de caracteres)
+    // Ej:
+    // stringMasLarga(['hi', 'hello', 'ni hao', 'guten tag']); debe retornar 'guten tag'
+    // stringMasLarga(['JavaScript', 'HTML', 'CSS']); debe retornar 'JavaScript'
+  
+    // Tu código aca:
+    let toNums = strings.map(e => e.length);
+    let maxNum = Math.max(...toNums);
+    return maxNum;
+
+  }
+  let strings = ['queso','pan'];
+  let res = strings(stringMasLarga)
+  console.log(res);
+  
