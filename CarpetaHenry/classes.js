879e46d326e4f0defda9c9bbabcd923dbe55229c
Kodama01 <jmtenorio1013@hotmnail.com>
2023-05-11 15:01:59 -0500

    na

--- CarpetaHenry/classes.js
+++ CarpetaHenry/classes.js
@@ -38,7 +38,7 @@
 // segundoAuto.info();
 
 
-//GETTERS & SETTERS
+//GETTERS & SETTERS (DEPRECATED)
 
 class Rectangle {
     constructor (_width,_height){
@@ -53,7 +53,7 @@ class Rectangle {
     set area(area){                         //SETTER
         this.width = Math.sqrt(area);
         this.height = this.width;
-    }
+    }                                                               //(GETTERS AND SETTERS) Deprecated: This feature is no longer recommended. Though some browsers might still support it, it may have already been removed from the relevant web standards, may be in the process of being dropped, or may only be kept for compatibility purposes. Avoid using it, and update existing code if possible; see the compatibility table at the bottom of this page to guide your decision. Be aware that this feature may cease to work at any time.
 }
 let rectangleOne = new Rectangle(3,5);    //GETTER
 console.log(rectangleOne.area)
@@ -65,4 +65,60 @@ console.log(rectangleOne.area);
 console.log(rectangleOne.numOfLogs);
 
 
+//STATIC METHODS
+class Square {
+    constructor(_width){
+        this.width = _width;
+        this.height = _width;
+    }
+    static equals (a , b){                                      
+        return (a.width * a.height === b.width * b.height)      
+    }
+    static validSquareDimensions (width,height){
+        return width === height;
+    }
+}
 
+let square1 = new Square(4);
+let square2 = new Square(4)
+console.log(square1);
+console.log(Square.equals(square1,square2));
+console.log(Square.validSquareDimensions(8,7));
+
+//INHERITANCE & EXTENDS
+
+
+//Parent class
+class Person {
+    constructor(_name,_age){
+        this.name = _name;
+        this.age = _age;
+    }
+}
+
+//Child class
+
+class Programmer extends Person {                           //Now a Programmer can be interpreted as a special kind of person or a sub class
+    constructor (_name,_age,_yearsOfExperience){
+        super(_name,_age);                                  //"super" keyword calls the constructor from the parent class "person"
+        //Custom Behaviour 
+        this.yearsOfExperience = _yearsOfExperience;
+    }
+    code(){
+        console.log(this.name + ' is coding')
+    }
+}
+
+let programmers  = [
+    new Programmer('Dominic',45,12),
+    new Programmer('Oconor',36,9),
+    new Programmer('David',39,5)
+]
+
+function developSoftware(programmers){
+    //Develop software
+    for(let prog of programmers){
+        prog.code();
+    }
+}
+developSoftware(programmers);
