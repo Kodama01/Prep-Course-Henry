4b1d6eff26dea7f45647af5f68c8939768d4e34f
Kodama01 <jmtenorio1013@hotmail.com>
2023-05-15 16:48:43 -0500

    na

---
+++ CarpetaHenry/listBinding.js
@@ -0,0 +1,15 @@
+class listBinding{
+    constructor(listElement){
+        this.listElement = listElement;
+        this.textList = [
+            'javascript',
+            'is a',
+            'weird programming',
+            'language'
+        ];
+    }
+    update(){
+        /* Remove all existing <li> elements/tags */
+    }
+
+}
