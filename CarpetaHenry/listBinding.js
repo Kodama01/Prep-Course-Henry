db82a89c4d0a5f24846d4e105197f90c2a1ed273
Kodama01 <jmtenorio1013@hotmail.com>
2023-05-15 23:28:48 -0500

    classexampl

--- CarpetaHenry/listBinding.js
+++ CarpetaHenry/listBinding.js
@@ -1,15 +1,32 @@
-class listBinding{
+class ListBinding{
     constructor(listElement){
         this.listElement = listElement;
-        this.textList = [
-            'javascript',
-            'is a',
-            'weird programming',
-            'language'
-        ];
+        this.textList = [];
+    }
+    /* Makes an <li>text</li> element/tag */
+    static createListItem (text){
+        let li = document.createElement('li')
+        li.textContent = text;
+        return li;
     }
     update(){
         /* Remove all existing <li> elements/tags */
+        while (this.listElement.firstChild){
+            this.listElement.removeChild(this.listElement.firstChild);
+        }
+        /* Fill <ul>/<ol> tag with <li> */
+        for (let text of this.textList){
+            this.listElement.appendChild(ListBinding.createListItem(text))
+        }
+    }
+
+    add(text){
+        this.textList.push(text);
+        this.update();
+    }
+    remove(index){
+        this.textList.splice(index, 1);
+        this.update();
     }
 
 }
